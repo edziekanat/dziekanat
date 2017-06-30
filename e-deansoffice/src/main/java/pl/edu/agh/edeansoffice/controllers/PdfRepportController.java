97fc39587321b0488c189ea62c5c59ca4199f0d6
wniziolek <wniziolek@gmail.com>
2017-06-30 16:12:52 +0200

    pdf report on workers account

--- e-deansoffice/src/main/java/pl/edu/agh/edeansoffice/controllers/PdfRepportController.java
+++ e-deansoffice/src/main/java/pl/edu/agh/edeansoffice/controllers/PdfRepportController.java
@@ -2,7 +2,10 @@ package pl.edu.agh.edeansoffice.controllers;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
@@ -10,30 +13,78 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import pl.edu.agh.edeansoffice.models.Employee;
 import pl.edu.agh.edeansoffice.models.GeneratePdfReport;
+import pl.edu.agh.edeansoffice.models.Student;
 import pl.edu.agh.edeansoffice.persistance.EmployeeDao;
+import pl.edu.agh.edeansoffice.persistance.StudentDao;
 
 @Controller
 public class PdfRepportController {
-	
+
 	@Autowired
 	private EmployeeDao employeesDao;
-	
-	@RequestMapping(value = "/pdfreport", method = RequestMethod.GET, produces = MediaType.APPLICATION_PDF_VALUE)
-	public ResponseEntity<InputStreamResource> employeesReport() throws IOException{
-		List<Employee> employees = (List<Employee>) employeesDao.findAll();
+	@Autowired
+	private StudentDao studentsDao;
+
+	@RequestMapping(value = "/reports")
+	public String displayEmployeesList(Model model, HttpSession session) {
+
+		model.addAttribute("message", "Test");
 		
-		ByteArrayInputStream bis = GeneratePdfReport.employeesReport(employees);
+		if (session.getAttribute("userLogin") == null) {
+    		return "redirect:/login";
+		} else {
+			return "reports";
+		}
+	}
+
+	@RequestMapping(value = "/createReport", method = RequestMethod.GET, produces = MediaType.APPLICATION_PDF_VALUE)
+	public ResponseEntity<InputStreamResource> employeesReport(Model model, @RequestParam(value="typeOfReport") String typeOfReport) throws IOException {
+		
+		ByteArrayInputStream bis = null;
+		
+		if (typeOfReport.equals("listOfStudents")){
+			List<Student> students = (List<Student>) studentsDao.findAll();
+			bis = GeneratePdfReport.studentsReport(students);
+		} else if(typeOfReport.equals("listOfWorkers")){
+			List<Employee> employees = (List<Employee>) employeesDao.findAll();
+			bis = GeneratePdfReport.employeesReport(employees);
+		}
+		
+/*		List<Student> students = (List<Student>) studentsDao.findAll();
+		
+		if (typeOfReport == "listOfStudents"){
+			model.addAttribute("message", "Test222");
+		}
+				
+		ByteArrayInputStream bis = GeneratePdfReport.studentsReport(students);*/
+		
 		
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Content-Disposition", "inline; filename=employeesreport.pdf");
-		
-		return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(new InputStreamResource(bis));
-				
+
+		return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF)
+				.body(new InputStreamResource(bis));
+
 	}
+	
+/*	@RequestMapping(value = "/createReport", method = RequestMethod.GET)
+	public String displaymessage(Model model, @RequestParam(value="typeOfReport") String typeOfReport) {
+		
+
+		if (typeOfReport.equals("listOfStudents")){
+			model.addAttribute("message", typeOfReport);
+		} else if(typeOfReport.equals("listOfWorkers")){
+			model.addAttribute("message", typeOfReport);
+		}
+		
+		return "reports";
+	}*/
 
 }
--- e-deansoffice/src/main/java/pl/edu/agh/edeansoffice/models/GeneratePdfReport.java
+++ e-deansoffice/src/main/java/pl/edu/agh/edeansoffice/models/GeneratePdfReport.java
@@ -80,5 +80,69 @@ public class GeneratePdfReport {
 
         return new ByteArrayInputStream(out.toByteArray());
     }
+	
+	
+	public static ByteArrayInputStream studentsReport(List<Student> students) {
+
+        Document document = new Document();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        try {
+
+            PdfPTable table = new PdfPTable(3);
+            table.setWidthPercentage(60);
+            table.setWidths(new int[]{1, 3, 3});
+
+            Font headFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+
+            PdfPCell hcell;
+            hcell = new PdfPCell(new Phrase("Id", headFont));
+            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(hcell);
+
+            hcell = new PdfPCell(new Phrase("Name", headFont));
+            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(hcell);
+
+            hcell = new PdfPCell(new Phrase("Population", headFont));
+            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(hcell);
+
+            for (Student student : students) {
+
+                PdfPCell cell;
+
+                cell = new PdfPCell(new Phrase(String.valueOf(student.getId())));
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                table.addCell(cell);
+
+                cell = new PdfPCell(new Phrase(student.getName()));
+                cell.setPaddingLeft(5);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+                table.addCell(cell);
+                
+                cell = new PdfPCell(new Phrase(student.getName()));
+                cell.setPaddingLeft(5);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+                table.addCell(cell);
+
+            }
+
+            PdfWriter.getInstance(document, out);
+            document.open();
+            document.add(table);
+            
+            document.close();
+            
+        } catch (DocumentException ex) {
+        
+            Logger.getLogger(GeneratePdfReport.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return new ByteArrayInputStream(out.toByteArray());
+    }
 
 }
